1751efe09e6ee724247dfe95f223f357d9033a48
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-10-20 12:38:32 -0400

    indents

--- othello/sdks/python/client.py
+++ othello/sdks/python/client.py
@@ -5,23 +5,23 @@ import json
 import socket
 
 def get_move(player, board):
-  # TODO determine valid moves
-  # TODO determine best move
-  print("Board:"+str(board))
-  print("Player:"+str(player))
-  me = player
-  #get valid move socket
-  validMoveList = getValidMoves(board, me)
-  markCorners(validMoveList)
-  print("Valid Moves: " + str(validMoveList))
-  # make a corner move if they exist
-  for gamePiece in validMoveList: # get the game piec
-    for choice in gamePiece: #get the move on that piece that is valid
-        if(choice[2] is 1):
-            return choice[0]
+    # TODO determine valid moves
+    # TODO determine best move
+    print("Board:"+str(board))
+    print("Player:"+str(player))
+    me = player
+    #get valid move socket
+    validMoveList = getValidMoves(board, me)
+    markCorners(validMoveList)
+    print("Valid Moves: " + str(validMoveList))
+    # make a corner move if they exist
+    for gamePiece in validMoveList: # get the game piec
+        for choice in gamePiece: #get the move on that piece that is valid
+            if(choice[2] is 1):
+                return choice[0]
 
-  print("Valid Moves: " + str(validMoveList))
-  return [4, 2] # (y, x) or (col, row)
+    print("Valid Moves: " + str(validMoveList))
+    return [4, 2] # (y, x) or (col, row)
 
 def markCorners(validMoveList):
     '''
@@ -180,25 +180,25 @@ def prepare_response(move):
   return response
 
 if __name__ == "__main__":
-  port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1337
-  host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname()
+    port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1337
+    host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname()
 
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  try:
-    sock.connect((host, port))
-    while True:
-      data = sock.recv(1024)
-      if not data:
-        print('connection to server closed')
-        break
-      json_data = json.loads(str(data.decode('UTF-8')))
-      board = json_data['board']
-      maxTurnTime = json_data['maxTurnTime']
-      player = json_data['player']
-      print(player, maxTurnTime, board)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((host, port))
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                print('connection to server closed')
+                break
+            json_data = json.loads(str(data.decode('UTF-8')))
+            board = json_data['board']
+            maxTurnTime = json_data['maxTurnTime']
+            player = json_data['player']
+            print(player, maxTurnTime, board)
 
-      move = get_move(player, board)
-      response = prepare_response(move)
-      sock.sendall(response)
-  finally:
-    sock.close()
+            move = get_move(player, board)
+            response = prepare_response(move)
+            sock.sendall(response)
+    finally:
+        sock.close()
